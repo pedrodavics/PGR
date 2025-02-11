837db2e1bf6b6654587734d7c4baf613d595b04b
4lbanes <joaoarthurvbd@gmail.com>
2025-02-11 11:23:29 -0300

    atualizando o inicio do processo de formatação

--- src/generator/formatter.py
+++ src/generator/formatter.py
@@ -64,22 +64,94 @@ def obter_dados_do_banco():
         conexao = jaydebeapi.connect('oracle.jdbc.driver.OracleDriver', jdbc_url, [user, password], jdbc_jar)
 
         consultas = {
-            "versao_do_banco_de_dados": "SELECT version FROM PRODUCT_COMPONENT_VERSION WHERE product LIKE 'Oracle Database%'",
-            "maiores_tabelas": """SELECT table_name, bytes/1024/1024/1024 AS tamanho_gb 
-                                  FROM dba_segments WHERE segment_type = 'TABLE'
-                                  ORDER BY tamanho_gb DESC FETCH FIRST 1 ROWS ONLY""",
-            "top_sql": """SELECT sql_text FROM v$sqlarea ORDER BY elapsed_time DESC FETCH FIRST 1 ROWS ONLY""",
-            "monitoramento_memoria": """SELECT resource_name, current_utilization FROM v$resource_limit WHERE resource_name = 'processes'""",
-            "monitoramento_cpu": """SELECT resource_name, current_utilization FROM v$resource_limit WHERE resource_name = 'sessions'""",
-            "crescimento_base": """SELECT round(sum(used.bytes) / 1024 / 1024 / 1024) || ' GB' AS tamanho
-                                   FROM (SELECT bytes FROM v$datafile UNION ALL
-                                         SELECT bytes FROM v$tempfile UNION ALL
-                                         SELECT bytes FROM v$log) used""",
-            "desvios_backup": """SELECT COUNT(*) FROM v$RMAN_BACKUP_JOB_DETAILS WHERE status != 'COMPLETED'""",
-            "data_backup": """SELECT MAX(start_time) FROM v$RMAN_BACKUP_JOB_DETAILS""",
-            "desvio_backup": """SELECT status FROM v$RMAN_BACKUP_JOB_DETAILS WHERE ROWNUM = 1 ORDER BY start_time DESC""",
-            "acao_backup": """SELECT time_taken_display FROM v$RMAN_BACKUP_JOB_DETAILS WHERE ROWNUM = 1 ORDER BY start_time DESC""",
-            "status_backup": """SELECT DISTINCT status FROM v$RMAN_BACKUP_JOB_DETAILS""",
+            "versao_do_banco_de_dados": """SELECT version 
+                                            FROM PRODUCT_COMPONENT_VERSION 
+                                            WHERE product LIKE 'Oracle Database%'""",
+
+            "usuarios_ativos": """SELECT username 
+                                  FROM dba_users 
+                                  WHERE account_status = 'OPEN' 
+                                  ORDER BY username""",
+
+            "recursos_do_banco": """SELECT resource_name, current_utilization, max_utilization 
+                                    FROM v$resource_limit 
+                                    WHERE resource_name IN ('processes', 'sessions')""",
+
+            "tamanho_espaco_banco": """SELECT 
+                                          round(sum(used.bytes) / 1024 / 1024 / 1024) || ' GB' AS "Database Size",
+                                          round(sum(used.bytes) / 1024 / 1024 / 1024) - 
+                                          round(free.p / 1024 / 1024 / 1024) || ' GB' AS "Used space",
+                                          round(free.p / 1024 / 1024 / 1024) || ' GB' AS "Free space"
+                                       FROM 
+                                          (SELECT bytes FROM v$datafile
+                                           UNION ALL
+                                           SELECT bytes FROM v$tempfile
+                                           UNION ALL
+                                           SELECT bytes FROM v$log) used,
+                                          (SELECT sum(bytes) AS p FROM dba_free_space) free
+                                       GROUP BY free.p""",
+
+            "maiores_tabelas": """SELECT * 
+                                  FROM 
+                                    (SELECT owner, segment_name AS table_name, bytes/1024/1024/1024 AS "SIZE (GB)"
+                                     FROM dba_segments 
+                                     WHERE segment_type = 'TABLE'
+                                     AND segment_name NOT LIKE 'BIN%' 
+                                     ORDER BY 3 DESC) 
+                                  WHERE rownum <= 20""",
+
+            "top_10_queries_lentas": """SELECT rownum AS rank, a.* 
+                                        FROM 
+                                          (SELECT elapsed_Time/1000000 AS elapsed_time, executions, cpu_time, sql_id, sql_text
+                                           FROM v$sqlarea 
+                                           WHERE elapsed_time/1000000 > 5 
+                                           ORDER BY elapsed_time DESC) a 
+                                        WHERE rownum < 11""",
+
+            "detalhes_backup_rman": """SELECT
+                                          TO_CHAR(j.start_time, 'yyyy-mm-dd hh24:mi:ss') AS start_time,
+                                          TO_CHAR(j.end_time, 'yyyy-mm-dd hh24:mi:ss') AS end_time,
+                                          (j.output_bytes/1024/1024) AS output_mbytes,
+                                          j.status, 
+                                          j.input_type,
+                                          DECODE(TO_CHAR(j.start_time, 'd'), 
+                                                 1, 'Sunday', 
+                                                 2, 'Monday', 
+                                                 3, 'Tuesday', 
+                                                 4, 'Wednesday', 
+                                                 5, 'Thursday', 
+                                                 6, 'Friday', 
+                                                 7, 'Saturday') AS dow,
+                                          j.elapsed_seconds, 
+                                          j.time_taken_display,
+                                          x.cf, 
+                                          x.df, 
+                                          x.i0, 
+                                          x.i1, 
+                                          x.l,
+                                          ro.inst_id AS output_instance
+                                        FROM v$RMAN_BACKUP_JOB_DETAILS j
+                                        LEFT OUTER JOIN 
+                                          (SELECT 
+                                             d.session_recid, 
+                                             d.session_stamp,
+                                             SUM(CASE WHEN d.controlfile_included = 'YES' THEN d.pieces ELSE 0 END) AS CF,
+                                             SUM(CASE WHEN d.controlfile_included = 'NO' AND d.backup_type||d.incremental_level = 'D' THEN d.pieces ELSE 0 END) AS DF,
+                                             SUM(CASE WHEN d.backup_type||d.incremental_level = 'D0' THEN d.pieces ELSE 0 END) AS I0,
+                                             SUM(CASE WHEN d.backup_type||d.incremental_level = 'I1' THEN d.pieces ELSE 0 END) AS I1,
+                                             SUM(CASE WHEN d.backup_type = 'L' THEN d.pieces ELSE 0 END) AS L
+                                           FROM v$BACKUP_SET_DETAILS d
+                                           JOIN v$BACKUP_SET s ON s.set_stamp = d.set_stamp AND s.set_count = d.set_count
+                                           WHERE s.input_file_scan_only = 'NO'
+                                           GROUP BY d.session_recid, d.session_stamp) x 
+                                        ON x.session_recid = j.session_recid AND x.session_stamp = j.session_stamp
+                                        LEFT OUTER JOIN 
+                                          (SELECT o.session_recid, o.session_stamp, MIN(inst_id) AS inst_id
+                                           FROM Gv$RMAN_OUTPUT o
+                                           GROUP BY o.session_recid, o.session_stamp) ro 
+                                        ON ro.session_recid = j.session_recid AND ro.session_stamp = j.session_stamp
+                                        WHERE j.start_time > TRUNC(SYSDATE)-7
+                                        ORDER BY j.start_time"""
         }
 
         dados = {}
--- src/scripts/sql/queries.sql
+++ src/scripts/sql/queries.sql
@@ -1,70 +1,95 @@
-SELECT version FROM PRODUCT_COMPONENT_VERSION WHERE product LIKE 'Oracle Database%';
+-- Consulta para a versão do Oracle
+SELECT version 
+FROM PRODUCT_COMPONENT_VERSION 
+WHERE product LIKE 'Oracle Database%';
 
-select username from dba_users where account_status='OPEN' order by username;
+-- Consulta para listar usuários ativos
+SELECT username 
+FROM dba_users 
+WHERE account_status = 'OPEN' 
+ORDER BY username;
 
-select resource_name, current_utilization, max_utilization from v$resource_limit where resource_name in ('processes','sessions');
+-- Consulta para recursos do banco de dados
+SELECT resource_name, current_utilization, max_utilization 
+FROM v$resource_limit 
+WHERE resource_name IN ('processes', 'sessions');
 
-select round(sum(used.bytes) / 1024 / 1024 / 1024) || ' GB' "Database Size",
-       round(sum(used.bytes) / 1024 / 1024 / 1024) -
-       round(free.p / 1024 / 1024 / 1024) || ' GB' "Used space",
-       round(free.p / 1024 / 1024 / 1024) || ' GB' "Free space"
-  from (select bytes
-          from v$datafile
-        union all
-        select bytes
-          from v$tempfile
-        union all
-        select bytes from v$log) used,
-       (select sum(bytes) as p from dba_free_space) free
- group by free.p;
+-- Consulta para o tamanho do banco de dados e espaço utilizado
+SELECT 
+  round(sum(used.bytes) / 1024 / 1024 / 1024) || ' GB' AS "Database Size",
+  round(sum(used.bytes) / 1024 / 1024 / 1024) - 
+  round(free.p / 1024 / 1024 / 1024) || ' GB' AS "Used space",
+  round(free.p / 1024 / 1024 / 1024) || ' GB' AS "Free space"
+FROM 
+  (SELECT bytes FROM v$datafile
+   UNION ALL
+   SELECT bytes FROM v$tempfile
+   UNION ALL
+   SELECT bytes FROM v$log) used,
+  (SELECT sum(bytes) AS p FROM dba_free_space) free
+GROUP BY free.p;
 
-select * from (select owner, segment_name table_name, bytes/1024/1024/1024 "SIZE (GB)" 
-from dba_segments where segment_type = 'TABLE'
-and segment_name not like 'BIN%' order by 3 desc) where rownum <= 20;
- 
-select rownum as rank, a.*
-from (
-select elapsed_Time/1000000 elapsed_time,
-executions,
-buffer_gets,
-disk_reads,
-cpu_time
-hash_value,
-sql_text
-from v$sqlarea
-where elapsed_time/1000000 > 5
-order by elapsed_time desc) a
-where rownum < 11;
+-- Consulta para as 20 maiores tabelas
+SELECT * 
+FROM 
+  (SELECT owner, segment_name AS table_name, bytes/1024/1024/1024 AS "SIZE (GB)"
+   FROM dba_segments 
+   WHERE segment_type = 'TABLE'
+   AND segment_name NOT LIKE 'BIN%' 
+   ORDER BY 3 DESC) 
+WHERE rownum <= 20;
 
-select
-to_char(j.start_time, 'yyyy-mm-dd hh24:mi:ss') start_time,
-to_char(j.end_time, 'yyyy-mm-dd hh24:mi:ss') end_time,
-(j.output_bytes/1024/1024) output_mbytes, j.status, j.input_type,
-decode(to_char(j.start_time, 'd'), 1, 'Sunday', 2, 'Monday',
-3, 'Tuesday', 4, 'Wednesday',
-5, 'Thursday', 6, 'Friday',
-7, 'Saturday') dow,
-j.elapsed_seconds, j.time_taken_display,
-x.cf, x.df, x.i0, x.i1, x.l,
-ro.inst_id output_instance
-from v$RMAN_BACKUP_JOB_DETAILS j
-left outer join (select
-d.session_recid, d.session_stamp,
-sum(case when d.controlfile_included = 'YES' then d.pieces else 0 end) CF,
-sum(case when d.controlfile_included = 'NO'
-and d.backup_type||d.incremental_level = 'D' then d.pieces else 0 end) DF,
-sum(case when d.backup_type||d.incremental_level = 'D0' then d.pieces else 0 end) I0,
-sum(case when d.backup_type||d.incremental_level = 'I1' then d.pieces else 0 end) I1,
-sum(case when d.backup_type = 'L' then d.pieces else 0 end) L
-from
-v$BACKUP_SET_DETAILS d
-join v$BACKUP_SET s on s.set_stamp = d.set_stamp and s.set_count = d.set_count
-where s.input_file_scan_only = 'NO'
-group by d.session_recid, d.session_stamp) x
-on x.session_recid = j.session_recid and x.session_stamp = j.session_stamp
-left outer join (select o.session_recid, o.session_stamp, min(inst_id) inst_id
-from Gv$RMAN_OUTPUT o
-group by o.session_recid, o.session_stamp)
-ro on ro.session_recid = j.session_recid and ro.session_stamp = j.session_stamp
-where j.start_time > trunc(sysdate)-7
-order by j.start_time;
+-- Consulta para top 10 queries mais lentas
+SELECT rownum AS rank, a.* 
+FROM 
+  (SELECT elapsed_Time/1000000 AS elapsed_time, executions, cpu_time, sql_id, sql_text
+   FROM v$sqlarea 
+   WHERE elapsed_time/1000000 > 5 
+   ORDER BY elapsed_time DESC) a 
+WHERE rownum < 11;
+
+-- Consulta para detalhes de backup do RMAN
+SELECT
+  TO_CHAR(j.start_time, 'yyyy-mm-dd hh24:mi:ss') AS start_time,
+  TO_CHAR(j.end_time, 'yyyy-mm-dd hh24:mi:ss') AS end_time,
+  (j.output_bytes/1024/1024) AS output_mbytes,
+  j.status, 
+  j.input_type,
+  DECODE(TO_CHAR(j.start_time, 'd'), 
+         1, 'Sunday', 
+         2, 'Monday', 
+         3, 'Tuesday', 
+         4, 'Wednesday', 
+         5, 'Thursday', 
+         6, 'Friday', 
+         7, 'Saturday') AS dow,
+  j.elapsed_seconds, 
+  j.time_taken_display,
+  x.cf, 
+  x.df, 
+  x.i0, 
+  x.i1, 
+  x.l,
+  ro.inst_id AS output_instance
+FROM v$RMAN_BACKUP_JOB_DETAILS j
+LEFT OUTER JOIN 
+  (SELECT 
+     d.session_recid, 
+     d.session_stamp,
+     SUM(CASE WHEN d.controlfile_included = 'YES' THEN d.pieces ELSE 0 END) AS CF,
+     SUM(CASE WHEN d.controlfile_included = 'NO' AND d.backup_type||d.incremental_level = 'D' THEN d.pieces ELSE 0 END) AS DF,
+     SUM(CASE WHEN d.backup_type||d.incremental_level = 'D0' THEN d.pieces ELSE 0 END) AS I0,
+     SUM(CASE WHEN d.backup_type||d.incremental_level = 'I1' THEN d.pieces ELSE 0 END) AS I1,
+     SUM(CASE WHEN d.backup_type = 'L' THEN d.pieces ELSE 0 END) AS L
+   FROM v$BACKUP_SET_DETAILS d
+   JOIN v$BACKUP_SET s ON s.set_stamp = d.set_stamp AND s.set_count = d.set_count
+   WHERE s.input_file_scan_only = 'NO'
+   GROUP BY d.session_recid, d.session_stamp) x 
+ON x.session_recid = j.session_recid AND x.session_stamp = j.session_stamp
+LEFT OUTER JOIN 
+  (SELECT o.session_recid, o.session_stamp, MIN(inst_id) AS inst_id
+   FROM Gv$RMAN_OUTPUT o
+   GROUP BY o.session_recid, o.session_stamp) ro 
+ON ro.session_recid = j.session_recid AND ro.session_stamp = j.session_stamp
+WHERE j.start_time > TRUNC(SYSDATE)-7
+ORDER BY j.start_time;
